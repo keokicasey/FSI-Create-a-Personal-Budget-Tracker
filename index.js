1a056f3078d1ddad66b2524655d948928b7a469a
Keoki <keoki.alan.mundon@gmail.com>
2021-11-15 19:33:00 -0800

    updated index.json

--- index.js
+++ index.js
@@ -2,4 +2,17 @@ let weeklyExpenses = 0
 let monthlyExpenses = 0
 let annualExpenses = 0
 
-// Your Code Here
+for (let i = 0; i <weeklyExpenseQuestions.length; i++) {
+    let answer = window.prompt(weeklyExpenseQuestions[i])
+    weeklyExpenses = weeklyExpenses + parseInt(answer)
+}
+
+for (let i = 0; i <monthlyExpenseQuestions.length; i++) {
+    let answer = window.prompt(monthlyExpenseQuestions[i])
+    monthlyExpenses = monthlyExpenses + parseInt(answer)
+}
+
+for (let i = 0; i <annualExpenseQuestions.length; i++) {
+    let answer = window.prompt(annualExpenseQuestions[i])
+    annualExpenses = annualExpenses + parseInt(answer)
+}
